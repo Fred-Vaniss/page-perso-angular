f33c476b554915146815798ae4880dc39c16071c
Fred-Vaniss <46893914+Fred-Vaniss@users.noreply.github.com>
2023-10-04 15:28:13 +0200

    Re-integrated AOS (animate on scroll)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Frédérick Van Isschot';
+
+  ngOnInit():void {
+
+    AOS.init({
+      duration: 800,
+      once: false,
+      easing: 'ease-out'
+    });
+    
+  }
 }
