d474f83e3329f25b5fc9282954e00f025e7f0f69
Fred-Vaniss <46893914+Fred-Vaniss@users.noreply.github.com>
2023-10-04 14:40:54 +0200

    Remade about section to avoid sanitization warnings

--- src/app/localization.ts
+++ src/app/localization.ts
@@ -8,25 +8,30 @@ export const LOCINTRO: Translation = {
 	en: "Web Developer"
 }
 
-export const LOCABOUT: Translation = {
-	fr: `
-		<h2>À propos de moi</h2>
-		<div className="about-text" data-aos="fade-up">
+interface LocAbout{
+	title: Translation;
+	text: Translation;
+}
+
+export const LOCABOUT: LocAbout = {
+	title: {
+		fr: "À propos de moi",
+		en: "About me"
+	},
+	text: {
+		fr: `
 			<p>J’ai fait mes débuts en tant qu’infographiste où j’ai appris plusieurs outils: retouche photo, web, animation, sonorisation, 3D, etc. </p>
 			<p>N'ayant que la base de tout, il m'a fallu choisir où me spécialiser. J'ai donc décidé de suivre la voie du développement web où j'ai suivi des cours en ligne en autodidacte avant de suivre des formations dans le web.</p>
 			<p>J'ai d'abord suivi une formation dans l'UX Webdesign où j'ai appris à concevoir des sites accessibles et de les développer en sites statiques. J'ai constaté que j'ai pris beaucoup plus de plaisir à les coder plutôt que de les concevoir, alors j'ai décidé de me spécialiser encore plus dans la programmation où j'ai appris beaucoup plus de choses et à me lancer dans des projets plus concrets.</p>
 			<p>Depuis, j'ai développé une vraie passion pour la programmation, j'adore fouiller un peu partout pour découvrir de nouvelles choses, de trouver des solutions et résoudre des problèmes.</p>
-		</div>
-	`,
-	en: `
-		<h2>About me</h2>
-		<div className="about-text" data-aos="fade-up">
+		`,
+		en: `
 			<p>I've begun as graphic designer where I learned multiple tools: photo editing, web, animation, sound systems, 3D, etc.</p>
 			<p>Having only the basics of everything, I had to specialize somewhere. So I decided to follow the path of web development where I took self-taught online courses before following web training courses.</p>
 			<p>I first followed UX Webdesign training course, where I learned to design accessible websites and develop static websites. I realized that I took more pleasure to code instead of designing, so I decided to specialize even more in programming where I learnt even more thing and get started to more concrete projects.</p>
 			<p>Since then, I developed a real passion on programming, I love to poke around and discover new things, finding solutions and solve problems.</p>
-		</div>
-	`
+		`
+	}
 }
 
 interface Locnavbar{
