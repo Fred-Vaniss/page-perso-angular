a9942d21bf7565f699d562695610851a5505f10c
Fred-Vaniss <46893914+Fred-Vaniss@users.noreply.github.com>
2024-03-19 10:56:52 +0100

    Added Hackathon, improved modal link opening

---
+++ src/app/directives/markdown-link.directive.spec.ts
@@ -0,0 +1,8 @@
+import { MarkdownModalLinkDirective } from './markdown-modal-link.directive';
+
+describe('MarkdownLinkDirective', () => {
+  it('should create an instance', () => {
+    const directive = new MarkdownModalLinkDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/directives/markdown-modal-link.directive.ts
@@ -0,0 +1,28 @@
+import {Directive, HostListener} from '@angular/core';
+import {ModalService} from "../modal.service";
+
+@Directive({
+  selector: '[appMarkdownModalLink]'
+})
+export class MarkdownModalLinkDirective {
+
+  constructor(
+    private modal : ModalService
+  ) { }
+
+  @HostListener("click", ["$event"])
+  public onClick(e: MouseEvent){
+    const srcElm = e.target;
+    if (srcElm instanceof HTMLAnchorElement){
+      const href = srcElm.href;
+
+      if(href.startsWith("modal:"))
+      {
+        const id = href.split(":")[1];
+        this.modal.openArticle(id);
+        e.preventDefault()
+        e.stopPropagation()
+      }
+    }
+  }
+}
